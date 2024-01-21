becc9e0e04777333f469a5f1bee7ac13b571dd2a
Shamil P P <56679389+shamilchennara84@users.noreply.github.com>
2024-01-21 18:42:26 +0530

    feat: Server Creation Completed
    ℹ️ Setup of the server has been successfully completed.

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./infrastructure/config/app");
+const db_1 = require("./infrastructure/config/db");
+const PORT = process.env.PORT || 3000;
+const app = (0, app_1.createServer)();
+(0, db_1.mongoConnect)()
+    .then(() => app === null || app === void 0 ? void 0 : app.listen(PORT, () => console.log(`listening to PORT ${PORT}`)))
+    .catch((err) => console.log("error while connecting to database\n", err));
