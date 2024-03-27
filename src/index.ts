2caa36893551aa5ea7256590e254aa9ee1494d88
Shamil P P <56679389+shamilchennara84@users.noreply.github.com>
2024-03-27 09:41:56 +0530

    fix(server): Add error handling for already used port
    Implemented error handling to gracefully handle cases where the specified port is already in use, preventing server startup failures.

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,9 @@ import { mongoConnect } from "./infrastructure/config/db";
 import http from "http";
 import { setupSocketIO } from "./infrastructure/config/socketIO";
 
-
-
 const PORT = process.env.PORT || 3000;
 
 const app = createServer();
-
-
 
 mongoConnect()
   .then(() => {
@@ -17,6 +13,14 @@ mongoConnect()
       const server = http.createServer(app);
       setupSocketIO(server);
       server.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
+
+      server.on("error", (error) => {
+        if ((error as NodeJS.ErrnoException).code === "EADDRINUSE") {
+          console.error(`Port ${PORT} is already in use. Please choose a different port.`);
+        } else {
+          console.error("Error starting server:", error);
+        }
+      });
     } else {
       throw Error("App is undefined");
     }
