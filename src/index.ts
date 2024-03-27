7ec2cd4bb3b7e1c5b4726cc1d48239302f52bfe5
Shamil P P <56679389+shamilchennara84@users.noreply.github.com>
2024-03-27 13:23:39 +0530

    triggering webhook-2

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ mongoConnect()
       const server = http.createServer(app);
       setupSocketIO(server);
       server.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
-
       server.on("error", (error) => {
         if ((error as NodeJS.ErrnoException).code === "EADDRINUSE") {
           console.error(`Port ${PORT} is already in use. Please choose a different port.`);
